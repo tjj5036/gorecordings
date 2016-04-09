e63259c59d8347ab215739fa8034470490814718
tjj5036 <tyler.jones@optimizely.com>
2016-04-09 12:12:47 -0700

    Add recording and concert tables respectively

--- schemas/concert_db.sql
+++ schemas/concert_db.sql
@@ -28,3 +28,28 @@ CREATE TABLE concert_setlist_mapping (
   concert_id                    integer NOT NULL references concerts(concert_id),
   songs                         integer ARRAY
 );
+
+CREATE TABLE recording_types (
+  recording_type_id             SERIAL PRIMARY KEY,
+  recording_name                varchar(50) NOT NULL
+);
+
+CREATE TABLE source_types (
+  source_type_id                SERIAL PRIMARY KEY,
+  source_name                   varchar(50)
+);
+
+CREATE TABLE recording (
+  recording_id                  SERIAL PRIMARY KEY,
+  recording_type                integer NOT NULL references recording_types(recording_type_id),
+  source_type                   integer NOT NULL references source_types(source_type_id),
+  taper                         varchar(100),
+  length                        integer,
+  notes                         text
+);
+
+CREATE TABLE concert_recording_mapping (
+  recording_id                  integer NOT NULL references recording(recording_id),
+  concert_id                    integer NOT NULL references concerts(concert_id),
+  UNIQUE (recording_id, concert_id)
+);
