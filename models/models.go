6fc89616628d1ff155d60406d11a1c76e105711b
tjj5036 <tyler.jones@optimizely.com>
2016-04-12 12:41:15 -0700

    Added function to get all artists from the database

--- models/models.go
+++ models/models.go
@@ -3,9 +3,16 @@ package models
 // Not really models, just a place to hold structs. They largely
 // correspond with the tables in the db schema.
 
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+)
+
 type _artist struct {
-	artist_name string
-	short_name  string
+	Artist_id   int
+	Artist_name string
+	Short_name  string
 }
 
 type _song struct {
@@ -36,4 +43,31 @@ type _concert struct {
 	setlist    *[]_song
 	recordings *[]_recording
 	notes      string
+}
+
+// GetArtist gets artist information from the database and returns
+// a list of artists
+func GetArtists(db *sql.DB) []_artist {
+	rows, err := db.Query("Select artist_id, artist_name, short_name FROM artists")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	artists := make([]_artist, 0)
+	for rows.Next() {
+		var artist_id int
+		var artist_name string
+		var short_name string
+		err = rows.Scan(&artist_id, &artist_name, &short_name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		artist_data := _artist{
+			Artist_id:   artist_id,
+			Artist_name: artist_name,
+			Short_name:  short_name,
+		}
+		artists = append(artists, artist_data)
+	}
+	return artists
 }
--- routes/artists/artists.go
+++ routes/artists/artists.go
@@ -4,26 +4,16 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/tjj5036/gorecordings/database"
-	"log"
+	"github.com/tjj5036/gorecordings/models"
 	"net/http"
 )
 
 // Lists all artists in the database
 func ArtistListing(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	db := database.CreateDBHandler()
-	rows, err := db.Query("Select artist_id, artist_name, short_name FROM artists")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-	for rows.Next() {
-		var artist_id int
-		var artist_name string
-		var short_name string
-		err = rows.Scan(&artist_id, &artist_name, &short_name)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Fprintf(w, "%v %v %v", artist_id, artist_name, short_name)
+	artists := models.GetArtists(db)
+	for i := 0; i < len(artists); i++ {
+		artist := artists[i]
+		fmt.Fprintf(w, "%v %v %v", artist.Artist_id, artist.Artist_name, artist.Short_name)
 	}
 }
